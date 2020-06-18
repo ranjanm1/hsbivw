1ae0bc8da634f4e0a6dcf3fc553b6c0c0115caf8
Ranjan Majumdar <ranjanm1@gmail.com>
2020-06-18 18:52:47 +0100

    elb tf file added

---
+++ elb-create.tf
@@ -0,0 +1,32 @@
+# Create a new load balancer
+resource "aws_elb" "bar" {
+  name               = "foobar-terraform-elb"
+  availability_zones = ["eu-west-2a", "eu-west-2b"]
+
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  instances                   = ["${aws_instance.foo.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "foobar-terraform-elb"
+  }
+}
